0194ead06a3245a4194b22d03c2156b257bae02a
medaminemghirbi <mohamed.amine.mghirbi@gmail.com>
2024-10-08 22:05:15 +0100

    fix smtp server production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,20 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "PFe2023_back_production"
+  config.action_mailer.delivery_method = :smtp
+  Rails.application.routes.default_url_options[:host] = 'https://doc-pro-tn.fly.dev'
+  host = 'https://doc-pro-tn.fly.dev' #replace with your own url
+  config.action_mailer.default_url_options = { host: host }
 
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => "mohamed.amine.mghirbi@gmail.com",
+    :password             => "weyveirgqntsmxrw",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+}
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
